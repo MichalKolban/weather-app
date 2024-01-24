2964cf895b1f4bf40bdb5723911110acf68cbbdc
MichalKolban <michal.kolban@gmail.com>
2024-01-24 23:45:37 +0100

    weather app ver 0.1.1

--- src/api/openWeatherApi.js
+++ src/api/openWeatherApi.js
@@ -1,21 +1,21 @@
 export const getData = () => {
-  const API_KEY = "21f52349a19f5008a4063add5d04eaec";
+  const API_KEY = "";
   const URL = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${API_KEY}`;
 
   // valid link !
-  // https://api.openweathermap.org/data/2.5/weather?lat=53.43&lon=14.59&appid=21f52349a19f5008a4063add5d04eaec
+  // https://api.openweathermap.org/data/2.5/weather?lat=53.43&lon=14.59&appid=
 
   //valid link po polski
-  // https://api.openweathermap.org/data/2.5/weather?lat=53.43&lon=14.59&lang=pl&units=metric&appid=21f52349a19f5008a4063add5d04eaec
+  // https://api.openweathermap.org/data/2.5/weather?lat=53.43&lon=14.59&lang=pl&units=metric&appid=
 
   //valid link + polski + celsjusz
-  // https://api.openweathermap.org/data/2.5/weather?lat=53.43&lon=14.59&lang=pl&units=metric&appid=21f52349a19f5008a4063add5d04eaec
+  // https://api.openweathermap.org/data/2.5/weather?lat=53.43&lon=14.59&lang=pl&units=metric&appid=
 };
 
 export const getCityCoordinates = (value) => {
-  const API_KEY = "21f52349a19f5008a4063add5d04eaec";
+  const API_KEY = "";
 
-  // http://api.openweathermap.org/geo/1.0/direct?q=Szczecin&limit=5&appid=21f52349a19f5008a4063add5d04eaec
+  // http://api.openweathermap.org/geo/1.0/direct?q=Szczecin&limit=5&appid=
 
   // const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=Szczecin&limit=5&appid=${API_KEY}`;
   const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${API_KEY}`;
